5af0f73085ff3cc67c3eb8d89bdcaeb0151de7f5
kriggus@gmail.com <kriggus@gmail.com@cf8d6537-50ee-795d-7d4b-1cc75748a4f0>
2014-05-29 19:17:22 +0000

    Lagt till ny klass för att ladda och ta hand om alla spelets Texture och TextureAtlas i.
    Ser till att texturer och regioner bara laddas en enda gång.

--- trunk/eclipse-workspace/gravity-navigator/src/se/fkstudios/gravitynavigator/ResourceDefs.java
+++ trunk/eclipse-workspace/gravity-navigator/src/se/fkstudios/gravitynavigator/ResourceDefs.java
@@ -4,14 +4,18 @@ package se.fkstudios.gravitynavigator;
  * Resource definitions.
  * @author kristofer
  */
+
 public final class ResourceDefs {
 	
 	/* Graphics */
-	/* File paths */
-	public static final String FILE_PATH_TEXTURE_PACK = "images/spritesheets/textures/textures.pack";
-	public static final String FILE_PATH_MAP_BACKGROUND_1 = "images/map_backgrounds/mab_background_01.png"; 
 	
-	/* Texture region names */
+	// indexes in TEXTURE_FILE_PATHS and TEXTURE_NAMES must match!
+	public static final String[] TEXTURE_FILE_PATHS = {"images/map_backgrounds/mab_background_01.png"}; 
+	public static final String[] TEXTURE_NAMES = {"map_background_01"}; 
+	
+	public static final String TEXTURE_PACK_FILE_PATH = "images/spritesheets/textures/textures.pack";
+	
+	/* Texture region names (in texture atlas) */
 	public static final String TEXTURE_REGION_NAME_SPACESHIP_PLAYER = "spaceship_player";
 	
 	/* Sound */
---
+++ trunk/eclipse-workspace/gravity-navigator/src/se/fkstudios/gravitynavigator/view/TextureLoader.java
@@ -0,0 +1,70 @@
+package se.fkstudios.gravitynavigator.view;
+
+import java.util.HashMap;
+
+import se.fkstudios.gravitynavigator.ResourceDefs;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Maps textures and texture regions to strings so that they are not loaded more than once. 
+ * 
+ * Implements the Singelton patterns. 
+ * 
+ * @author kristofer
+ */
+public class TextureLoader {
+
+	private static final TextureLoader instance = new TextureLoader();
+	
+	private HashMap<String, Texture> textureMapper;
+	private TextureAtlas textureAtlas;
+	
+	private TextureLoader() {
+		
+		textureAtlas = new TextureAtlas(Gdx.files.internal(ResourceDefs.TEXTURE_PACK_FILE_PATH));
+		
+		textureMapper = new HashMap<>(ResourceDefs.TEXTURE_FILE_PATHS.length);
+		for (int i = 0; i < ResourceDefs.TEXTURE_FILE_PATHS.length; i++) {
+
+			String name = ResourceDefs.TEXTURE_NAMES[i];
+			String path = ResourceDefs.TEXTURE_FILE_PATHS[i];
+			Texture texture = new Texture(Gdx.files.internal(path));
+			textureMapper.put(name, texture);
+		}
+	}
+	
+	/**
+	 * Gets texture stored in game's texture atlas. Texture region names should be constants 
+	 * in ResourcesDefs.TEXTURE_NAMES.
+	 * 
+	 * @param textureName the name of the texture to search for.
+	 * @return the texture with the given name, null if not found.
+	 */
+	public Texture getTexture(String textureName) {
+		return textureMapper.get(textureName);
+	}
+	
+	/**
+	 * Gets texture region stored in game's texture atlas. Texture region names should be constants 
+	 * in ResourcesDefs. Regions names also matches the file names of the .PNG files when running 
+	 * the texture packer. 
+	 * 
+	 * @param textureRegionName the name of the texture region to search for.
+	 * @return the texture region with the given name, null if not found.
+	 */
+	public TextureRegion getTextureRegion(String textureRegionName) {
+		return textureAtlas.findRegion(textureRegionName);
+	}
+	
+	/**
+	 * Returns the only allowed instance of this object.
+	 * @return self
+	 */
+	public static TextureLoader getInstance() {
+		return instance;
+	}
+}
