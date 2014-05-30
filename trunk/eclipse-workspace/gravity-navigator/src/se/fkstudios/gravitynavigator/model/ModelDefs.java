2b936cecb1021be4651d54a8b3019e2ddd514a06
fhvjohnsson@gmail.com <fhvjohnsson@gmail.com@cf8d6537-50ee-795d-7d4b-1cc75748a4f0>
2014-05-30 14:27:12 +0000

    Lade till en ModelDefs. Med konstanter för bl.a startposition

---
+++ trunk/eclipse-workspace/gravity-navigator/src/se/fkstudios/gravitynavigator/model/ModelDefs.java
@@ -0,0 +1,16 @@
+package se.fkstudios.gravitynavigator.model;
+
+import com.badlogic.gdx.math.Vector2;
+
+/*
+ * Contains model-specific definitions such as physical constants, 
+ * conversion factors, initial states etc.
+ * @author fredrik
+ * 
+ */
+public final class ModelDefs {
+	
+	public static Vector2 STARTING_POSITION = new Vector2(0,0);
+	public static Vector2 STARTING_VELOCITY = new Vector2(0,0);
+
+}
