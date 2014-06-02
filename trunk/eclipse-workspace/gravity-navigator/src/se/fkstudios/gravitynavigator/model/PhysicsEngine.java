2fa0637c28319d2837e4bda629830b5e832f36ec
fhvjohnsson@gmail.com <fhvjohnsson@gmail.com@cf8d6537-50ee-795d-7d4b-1cc75748a4f0>
2014-06-02 14:38:18 +0000

    Lagt till "skalär" beräkningsfunktionalitet.

--- trunk/eclipse-workspace/gravity-navigator/src/se/fkstudios/gravitynavigator/model/PhysicsEngine.java
+++ trunk/eclipse-workspace/gravity-navigator/src/se/fkstudios/gravitynavigator/model/PhysicsEngine.java
@@ -11,7 +11,14 @@ import com.badlogic.gdx.utils.Array;
  */
 public class PhysicsEngine {
 	
-	public static MapObjectModel[] allMapObjects;
+	private static MapObjectModel[] allMapObjects;
+	private static float mapWidth;
+	private static float mapHeight;
+	
+	private static void setMapDimensions(Vector2 v) {
+		mapWidth = v.x;
+		mapHeight = v.y; 
+	}
 	
 	public static void setMapObjects (MapObjectModel m[]) {
 		allMapObjects = m; 
@@ -67,14 +74,81 @@ public class PhysicsEngine {
 		return direction;
 	}
 	
+	private static Vector2 computeGravitationalForce (Vector2 p1, int m1, Vector2 p2, int m2)
+	{
+		float G = ModelDefs.GRAVITATIONAL_CONSTANT; 
+		float distance = p1.dst(p2); 
+		float f;
+		
+		if (distance != 0) {
+			f = G*m1*m2/(distance*distance); // Newtons law of gravity
+			
+		}
+		else 
+			f = 0;
+		
+		// gives the size of the force. Now we'll need the direction
+		Vector2 diff = p1.sub(p2);
+		Vector2 direction;
+		if (diff.dot(diff) != 0) {
+			direction = diff.div((float) Math.sqrt(diff.dot(diff)));
+		}
+		else
+			direction = new Vector2(0,0);
+		return direction.cpy();
+	}
+	
 	/*
 	 * Computes acceleration of an object due to gravity by integrating with respect to some small time delta
 	 * The reason I'm not nesting these computations in one function is simply to separate velocities 
 	 * accelerations, forces etc. 
 	 */
 	private static Vector2 computeAcceleration(MapObjectModel o1, MapObjectModel o2, float delta) {
-		return computeGravitationalForce(o1,o2).scl(delta); 
+		return computeGravitationalForce(o1,o2).scl(delta); 	
+	}
+	
+	private static Vector2 computeAcceleration(Vector2 p1, int m1, Vector2 p2, int m2) {
+		return computeGravitationalForce(p1,m1,p2,m2);
+	}
+	
+	private static float computeScalarGravitationalForce (int m1,int m2,float d) {
+	if (d != 0) {
+		float G = ModelDefs.GRAVITATIONAL_CONSTANT; 
+		return G*m1*m2/(d*d); // Newtons law of gravity
 		
 	}
+	else 
+		return 0;
+	}
+	
+/*// brainfuck, fixar sen 
+	private static Vector2 computePeriodicCompensation(MapObjectModel o1, MapObjectModel o2, float delta)  {
+		Vector2 accel = computeAcceleration(o1,o2,delta);
+		float x = accel.x;
+		float y = accel.y; 
+		Vector2 p1 = o1.getPosition();
+		Vector2 p2 = o2.getPosition(); 
+		int m1 = o1.getMass();
+		int m2 = o2.getMass(); 
+		int ansX;
+		int ansY; 
+		
+		if (x> 0 && y >0) {
+			Vector2 a = computeAcceleration(p1,m1,p2,m2)
+			
+		}	
+		else if (x>0 && y<0) {
+			
+		}	
+		else if (x<0 && y>0) {
+			
+		}	
+		else if (x<0 && y <0) {
+			
+		}
+				
+		
+	}
+	*/
 	
 }
