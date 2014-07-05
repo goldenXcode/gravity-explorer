749077ae50eb6beffc7641dd523d7c6845e0a532
fhvjohnsson@gmail.com <fhvjohnsson@gmail.com@cf8d6537-50ee-795d-7d4b-1cc75748a4f0>
2014-07-05 19:43:02 +0000

    Lagt till en randomMapGenerator. Kan i nuläget generera random asteroider med valfri storlek

---
+++ trunk/eclipse-workspace/gravity-navigator/src/se/fkstudios/gravitynavigator/model/RandomMapGenerator.java
@@ -0,0 +1,61 @@
+package se.fkstudios.gravitynavigator.model;
+
+import java.util.Random;
+import se.fkstudios.gravitynavigator.ResourceDefs;
+import com.badlogic.gdx.math.Vector2;
+
+public class RandomMapGenerator {
+	
+	private static float objectDensity = 1273f; // calculated from asteroid1 (just for reference).
+	
+	/*
+	 * Inputs  a PeriodicMapModel and generates a random map on it. 
+	 */
+	public static PeriodicMapModel generateMap(PeriodicMapModel map) {
+		return map; 
+	}
+	
+	public static TextureMapObjectModel generateAsteroid (float radius) {
+		int mass = (int) calculateMass(radius);
+		TextureMapObjectModel asteroid = new TextureMapObjectModel(new Vector2((float)randomInt(0,10), (float)randomInt(0,5)), radius, radius, 
+				new Vector2(0.2f, 0.0f), 0, mass, ResourceDefs.TEXTURE_REGION_NAME_ASTERIOID_01);
+		// density of object = 1273 kg/m² 
+		return asteroid; 
+	}
+	
+//	public static MapObjectModel generatePlanet () {
+//		
+//	}
+	
+	
+	
+	/*
+	 * Assumes homogenous mass distribution and calculates a (circular) objects mass given it's radius.  
+	 */
+	private static float calculateMass(float radius) {
+
+		return (float) (Math.pow(radius, 2)*Math.PI*objectDensity); 
+		
+	}
+	
+	public static void setObjectDensity(float density) {
+		objectDensity = density; 
+		
+	}
+	
+	public static float getObjectDensity() {
+		return objectDensity; 
+	}
+	
+	public static int randomInt(int min, int max) {
+
+	    Random rand = new Random();
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
+	
+	
+	
+
+}
