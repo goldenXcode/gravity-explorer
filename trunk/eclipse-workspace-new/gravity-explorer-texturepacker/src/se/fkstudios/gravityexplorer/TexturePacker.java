f721c179a73521b12ed94ee93b3efcaa1946bfe1
kriggus@gmail.com <kriggus@gmail.com@cf8d6537-50ee-795d-7d4b-1cc75748a4f0>
2014-11-16 20:58:26 +0000

    Added the texture packer to new workspace.

---
+++ trunk/eclipse-workspace-new/gravity-explorer-texturepacker/src/se/fkstudios/gravityexplorer/TexturePacker.java
@@ -0,0 +1,35 @@
+package se.fkstudios.gravityexplorer;
+
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
+
+/**
+ * Tiny program using libGDX's imagepacker to combine the game's images into one composite sprite sheet (one large 
+ * image). Specifically, the images in '/gravity-explorer/android/assets/images' are combine into one in
+ * '/gravity-explorer/android/assets/images/textures'.  
+ * 
+ * The TextureAtlas and TextureRegion classes are then used in the game to access individual images. 
+ * 
+ * NOTE: OpenGL runs faster using fewer textures. This because when streaming coordinates (for calculate the texture section 
+ * to display) to the vertex shaders, the graphics hardware does not need to switch textures (e.i. reloading shaders) 
+ * like when using separate textures files.
+ * 
+ * @author kristofer
+ */
+public class TexturePacker {
+	
+	public static void main(String[] args) {
+		
+		Settings settings = new Settings();
+		
+		// not all devices have support for bigger textures (without losing performance). 
+		// http://stackoverflow.com/questions/17292404/texture-packer-size-issue-in-libgdx
+		settings.maxHeight = 1024;
+		settings.maxWidth = 1024;
+	
+		TexturePacker2.process(settings, 
+				"../gravity-explorer/android/assets/images/spritesheets", 
+				"../gravity-explorer/android/assets/images/spritesheets/textures", 
+				"textures.pack");
+	}
+}
